dac232e58c8a95cff5c0e61a1cb5a338b96f4da7
anviks <anviks@ttu.ee>
2025-06-07 21:10:35 +0300

    Add interfaces for repositories

---
+++ UnoGame.Core/Interfaces/IGameRepository.cs
@@ -0,0 +1,12 @@
+using UnoGame.Core.Entities;
+
+namespace UnoGame.Core.Interfaces;
+
+public interface IGameRepository
+{
+    public Task<List<Game>> GetAllGames();
+    public Task<Game?> GetGame(int id);
+    public Task<Game?> GetGameByName(string name);
+    public Task<Game> CreateGame(Game game);
+    public Task DeleteGame(int id);
+}
