c563769e629e96f8408ea19fc9d80fcd1ff003dd
anviks <117015417+anviks@users.noreply.github.com>
2025-12-26 22:59:32 +0200

    Rename UserDto field Name to Username

--- backend/UnoGame.Core/DTO/UserDto.cs
+++ backend/UnoGame.Core/DTO/UserDto.cs
@@ -3,5 +3,5 @@ namespace UnoGame.Core.DTO;
 public class UserDto
 {
     public int Id { get; set; }
-    public string Name { get; set; } = default!;
+    public string Username { get; set; } = default!;
 }
